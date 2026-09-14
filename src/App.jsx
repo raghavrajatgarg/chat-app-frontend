dd279054d6ae40afd07da940375877b437f85c67
Raghav Rajat Garg <raghav.scts@gmail.com>
2026-09-14 21:54:39 +0530

    Point to live Render backend

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:5000');
+const socket = io('https://chat-app-backend-1yfa.onrender.com');
 
 function App() {
   const [username, setUsername] = useState('');
