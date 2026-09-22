ded8a3e5a46b6324702c9c2ec66b2b0a2c1f6984
Raghav <raghav.scts@gmail.com>
2026-09-22 19:01:36 +0530

    threads 3

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -183,6 +183,16 @@ export default function ChatFeed({
                               <path fillRule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/>
                             </svg>
                           </button>
+                          <button 
+  onClick={() => setActiveThreadMessage(msg)} 
+  className={styles.threadReplyTriggerBtn}
+  title="Reply in thread"
+>
+  <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" viewBox="0 0 16 16">
+    <path d="M5.921 11.9 1.353 8.62a.719.719 0 0 1 0-1.238L5.921 4.1A.716.716 0 0 1 7 4.719V6c1.5 0 6 0 7 8-2.5-4.5-7-4-7-4v1.281c0 .56-.606.898-1.079.62z"/>
+  </svg>
+  <span>Reply</span>
+</button>
                       
                           {isMenuOpen && (
                             <div className={styles.dropdownMenu}>
